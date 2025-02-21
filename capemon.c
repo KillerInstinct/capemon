5aa9d4a26978d560c76579357d6d5d0f472c2c00
KillerInstinct <hodges92@hotmail.com>
2025-02-20 20:23:30 -0500

    Math is hard

--- capemon.c
+++ capemon.c
@@ -701,7 +701,7 @@ BOOL APIENTRY DllMain(HANDLE hModule, DWORD dwReason, LPVOID lpReserved)
 #ifndef _WIN64
 		if (!g_config.no_stealth) {
 			PEB *peb = get_peb();
-			if (peb->NumberOfProcessors == SPOOFED_CPU_CORE_NUM)
+			if (peb->NumberOfProcessors < SPOOFED_CPU_CORE_NUM)
 				peb->NumberOfProcessors = SPOOFED_CPU_CORE_NUM;
 		}
 #endif
